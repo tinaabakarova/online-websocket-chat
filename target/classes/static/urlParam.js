518a8828ebc2161d7f970e8ceccebf85979cec1a
Fatima Abakarova <f.abakarova@exite.ru>
2021-08-22 18:45:41 +0300

    . added gitignore file

--- target/classes/static/urlParam.js
+++
@@ -1,7 +0,0 @@
-$.urlParam = function(name, href){
-    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(href);
-    if (results==null) {
-       return null;
-    }
-    return decodeURI(results[1]) || 0;
-}
